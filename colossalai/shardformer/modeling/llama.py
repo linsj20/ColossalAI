fb85479902643cdfe6a87dd9acc3fe4540721c56
linsj20 <linsj20@mails.tsinghua.edu.cn>
2024-01-26 16:16:05 +0800

    support mode 2 sp in gpt2 (#5)
    * [shardformer] add megatron sp to llama
    
    * support llama7B 128k with distributed attention
    
    * [shardformer] robustness enhancement
    
    * add block attn
    
    * sp mode 1: keep input as a complete sequence
    
    * fix sp compatability
    
    * refactor ring implementation
    
    * support mode 2 sp in gpt2

--- colossalai/shardformer/modeling/llama.py
+++ colossalai/shardformer/modeling/llama.py
@@ -24,6 +24,8 @@ from colossalai.shardformer.layer._operation import (
     gather_forward_split_backward,
     reducescatter_forward_gather_backward,
     split_forward_gather_backward,
+    gather_forward_reducescatter_backward,
+    reducescatter_forward_gather_backward,
 )
 from colossalai.shardformer.shard import ShardConfig
 
@@ -443,7 +445,6 @@ class LlamaPipelineForwards:
             hidden_states = transformer_outputs.get("hidden_states")
             return {"hidden_states": hidden_states}
 
-
 def get_llama_flash_attention_forward(shard_config):
     from transformers.models.llama.modeling_llama import LlamaAttention, apply_rotary_pos_emb
 
@@ -534,8 +535,8 @@ def get_llama_flash_attention_forward(shard_config):
                     )
                 flash_attention_mask = ~(attention_mask[:, :, -1].squeeze(1).to(torch.bool)).contiguous()
             attn_mask_type = AttnMaskType.paddedcausal
-        hidden_size = self.hidden_size // sp_size if sp_mode == "3" else self.hidden_size
-
+        hidden_size = self.hidden_size // sp_size if sp_mode == '3' else self.hidden_size
+        
         attention = ColoAttention(embed_dim=hidden_size, num_heads=self.num_heads)
         attn_output = attention(
             query_states,
@@ -882,7 +883,7 @@ def get_llama_seq_parallel_attention_forward(sp_mode, sp_size, sp_group):
         # TODO (linshengjie) Block attention with ring
         ####
         block_wise = False
-        seq_len = query_states[2]
+        seq_len = query_states.shape[2]
         seq_block = 1024
         if block_wise and seq_len > seq_block:
             assert query_states.shape[2] % seq_block == 0
@@ -1032,6 +1033,7 @@ def get_llama_seq_parallel_model_forward(sp_mode, sp_size, sp_group):
 
         return combined_attention_mask
 
+
     def forward(
         self,
         input_ids: torch.LongTensor = None,
@@ -1158,6 +1160,7 @@ def get_llama_seq_parallel_model_forward(sp_mode, sp_size, sp_group):
                     position_ids,
                 )
             else:
+                
                 layer_outputs = decoder_layer(
                     hidden_states,
                     attention_mask=attention_mask,
